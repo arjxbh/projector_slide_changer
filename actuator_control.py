dcfd57666c7554d0c97b421238b9121ec7a01350
Arjun Bhatnagar <arjxbh@gmail.com>
2025-11-24 10:18:53 -0500

    adjust cycle time to account for a longer actuator

--- actuator_control.py
+++ actuator_control.py
@@ -33,8 +33,8 @@ RELAY_CHANNEL_2 = 3   # GPIO pin for relay channel 2
 
 # Timing configuration (in seconds)
 INITIAL_RETRACT_TIME = 1.0   # Initial retraction on startup
-CYCLE_EXTEND_TIME = 3.5      # Extend time in cycle
-CYCLE_RETRACT_TIME = 3.5     # Retract time in cycle
+CYCLE_EXTEND_TIME = 4.5      # Extend time in cycle
+CYCLE_RETRACT_TIME = 4.5     # Retract time in cycle
 STOP_DELAY = 0.1             # Stop delay between extend/retract (100ms)
 CYCLE_WAIT_TIME = 10.0       # Wait time between cycles
 
